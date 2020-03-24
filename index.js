4cc28e2cccf1732c3c7148cf187517d56dd4b648
nir <a>
2020-03-24 17:53:01 +0200

    Added loading scripts to index.js and popup after X time

--- index.js
+++ index.js
@@ -8,4 +8,27 @@ const header = "header";
 loadHTML(header, header, `${header}.html`);
 
 const twoPhotos = "two-photos";
-loadHTML(twoPhotos, twoPhotos, `${twoPhotos}.html`);
+loadHTML(twoPhotos, twoPhotos, `${twoPhotos}.html`);
+
+const vaccineStatusPerLab = "vaccine-status-per-lab";
+loadHTML(vaccineStatusPerLab, vaccineStatusPerLab, `${vaccineStatusPerLab}.html`);
+
+const vaccinePrecentagePerLab = "vaccine-percentage-per-lab";
+loadHTML(vaccinePrecentagePerLab, vaccinePrecentagePerLab, `${vaccinePrecentagePerLab}.html`);
+
+function setAlertUptimeTimeout() {
+    setTimeout(alertUptimeTimoutCallback, 3000);
+}
+
+function alertUptimeTimoutCallback() {
+    let alertModal = document.createElement("alert-modal");
+    alertModal.message = "והבדידות והבדידים החברים היחידים שנשארו לי";
+    alertModal.onClickOK = () => {
+        document.body.removeChild(alertModal);
+        setAlertUptimeTimeout();
+        //$("alert-modal").remove();
+    };
+    document.body.appendChild(alertModal);
+}
+
+setAlertUptimeTimeout();
