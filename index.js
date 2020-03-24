00b48fa96e1845a6a3d9e00cd4989a21c11ed2e0
nir <a>
2020-03-24 17:50:36 +0200

    updated tow-photos

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ const header = "header";
 loadHTML(header, header, `${header}.html`);
 
 const twoPhotos = "two-photos";
-loadHTML(twoPhotos, twoPhotos, `${twoPhotos}.html`);
+loadHTML(twoPhotos, twoPhotos, `${twoPhotos}.html`);
