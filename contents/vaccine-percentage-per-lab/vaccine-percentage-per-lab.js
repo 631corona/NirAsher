34073ab94ddbf856cac22a13a080024e10df3c63
nir <a>
2020-03-24 17:54:34 +0200

    Added vaccine-percentage-per-lab

---
+++ contents/vaccine-percentage-per-lab/vaccine-percentage-per-lab.js
@@ -0,0 +1,51 @@
+$.get("http://localhost:3000/api/v1/labs", (data, status) => {
+    //return data.labs.map(lab => lab.name);
+    //console.log(labsNames)
+}).done(res => createChart(res.labs));
+
+
+
+
+function createChart(labs){
+    const labsNames = labs.map(lab => lab.name);
+    const labsPercentile = labs.map(lab => lab.progress_percentile);
+    console.log(labsNames);
+    let ctx = document.getElementById('vaccine-percentage-per-lab-chart').getContext('2d');
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labsNames,
+            datasets: [{
+                label: 'Vaccine Development Percentage',
+                //data: [, 19, 3, 5, 2, 3],
+                data: labsPercentile,
+                backgroundColor: [
+                    // 'rgba(255, 99, 132, 0.2)',
+                    // 'rgba(54, 162, 235, 0.2)',
+                    // 'rgba(255, 206, 86, 0.2)',
+                    // 'rgba(75, 192, 192, 0.2)',
+                    // 'rgba(153, 102, 255, 0.2)',
+                    // 'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    // 'rgba(255, 99, 132, 1)',
+                    // 'rgba(54, 162, 235, 1)',
+                    // 'rgba(255, 206, 86, 1)',
+                    // 'rgba(75, 192, 192, 1)',
+                    // 'rgba(153, 102, 255, 1)',
+                    // 'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                    }
+                }]
+            }
+        }
+    });
+}
