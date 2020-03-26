83a8228b9cfc964f034f71242d416306664fa839
nir <a>
2020-03-26 13:23:13 +0200

    Fixed navbar resizing

--- client/contents/header/header.js
+++ client/contents/header/header.js
@@ -1,19 +1,44 @@
-$(window).click((e) => {
-    if($(".navbar-collapse").hasClass("show")){
-        $('.navbar-collapse').removeClass("show");
+const navbarCollapseSelector = ".navbar-collapse";
+const navbarMenuShowClass = "show";
+
+function getNavbarHeight(){
+    if ($(navbarCollapseSelector).hasClass(navbarMenuShowClass)){
+        return $("nav").outerHeight() - $(".show").outerHeight();
+    }
+    return $(".navbar").outerHeight();
+}
+
+function updateContentPadding(){
+    $(".content").css("padding-top", `${getNavbarHeight()}px`);
+}
+
+function endResizing(){
+    // Haven't resized in 0.5 sec!
+    updateContentPadding();
+    console.log("end resizing")
+}
+
+$(window).click(() => {
+    if($(navbarCollapseSelector).hasClass(navbarMenuShowClass)){
+        $(navbarCollapseSelector).removeClass(navbarMenuShowClass);
     }
 });
 
-var nav = $('.content-nav');
+updateContentPadding();
 
-
-var elements = document.getElementsByTagName('a');
-for(var i = 1; i < elements.length; i++) {
+let elements = document.getElementsByTagName('a');
+for(let i = 1; i < elements.length; i++) {
     elements[i].onclick = function() {
-        var hash = this.hash.substr(1),
+        let hash = this.hash.substr(1),
             elementTop = document.getElementById(hash).offsetTop;
-        window.scrollTo(0, elementTop - 56);
+        window.scrollTo(0, elementTop - getNavbarHeight());
         window.location.hash = '';
         return false;
     }
 }
+
+let resizingTimeout;
+$(window).resize(() => {
+    clearTimeout(resizingTimeout);
+    resizingTimeout = setTimeout(endResizing, 500);
+});
